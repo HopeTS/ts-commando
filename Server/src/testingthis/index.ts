93c78459745cb3a896d402b29173d85bfd36224f
Robby Bergers <bergersr97@gmail.com>
2022-04-29 11:57:09 -0400

    its a working server

--- Server/src/testingthis/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-export default function (app: express.Express) {
-  app.get("/shit", (req, res) => {
-    console.log("I guess man");
-    res.send("Please for the love of christ");
-  });
-}
