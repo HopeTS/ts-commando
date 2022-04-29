4330066556e9a9880a347cf20da4024e56c5f718
Robby Bergers <bergersr97@gmail.com>
2022-04-29 09:58:05 -0400

    #1 add basic server, webpack is broken downgrade to v4

---
+++ Server/webpack.dev.js
@@ -0,0 +1,21 @@
+const webpackCommon = require("./webpack.common");
+
+module.exports = {
+  mode: "development",
+  target: webpackCommon.target,
+  entry: webpackCommon.entry,
+  output: webpackCommon.output,
+  resolve: webpackCommon.resolve,
+  module: {
+    rules: [
+      ...webpackCommon.module.rules,
+      { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
+    ],
+  },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 600,
+    ignored: /node_modules/,
+  },
+  plugins: [...webpackCommon.plugins],
+};
